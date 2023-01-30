e896bc6ce4eb8c70d409706708d88b0194d64a3c
Shamaun-Nabi <snashik950@gmail.com>
2023-01-30 21:56:19 +0600

    getting response from api

--- index.js
+++ index.js
@@ -11,8 +11,35 @@ env.config();
 app.use(cors());
 app.use(bodyParser.json());
 
+// setup configuration
+const configuration = new Configuration({
+  organization: "org-uKAT93Pa6rw9QwkstofKZAtm",
+  apiKey: process.env.API_KEY,
+});
+const openAi = new OpenAIApi(configuration);
+// setup configuration done
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// post route
+
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+  console.log(message);
+  try {
+    const response = await openAi.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${message}`,
+      max_tokens: 100,
+      temperature: 0.5,
+    });
+    res.send({ message: response.data.choices[0].text });
+  } catch (e) {
+    // console.log(e);
+    res.send(e).status(400);
+  }
 });
 
 app.listen(port, () => {
